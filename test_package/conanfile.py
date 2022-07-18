d480576db8618542fea4b44b28d81d2aa71e4517
wangqiwen <isqiwen@gnail.com>
2022-07-18 11:25:02 +0800

    fix bug for windows

---
+++ test_package/conanfile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, CMake
+import os
+
+
+class TestPackageConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.so", dst="bin", src="lib")
+
+    def test(self):
+        os.chdir("bin")
+        self.run(".%stest_package" % os.sep)
